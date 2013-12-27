3ed372faade29c7c0ed6a3591c807b89e66ccae2
Peter Casinelli <pcas00@gmail.com>
2013-12-27 11:08:02 -0500

    Added index view and controller function that print ip address

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', ip_address = request.remote_addr)
+
+if __name__ == "__main__":
+    app.run()
