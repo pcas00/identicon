588f76ee118958b30ca19d85c09f4e14a21eb1c0
Peter Casinelli <pcas00@gmail.com>
2013-12-27 11:31:13 -0500

    Added hashed ip and index view displays two identicons

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask, request, render_template
+import hashlib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', ip_address = request.remote_addr)
+	m = hashlib.md5()
+	m.update(request.remote_addr)
+	hashed_ip = m.hexdigest()
+	return render_template('index.html', ip_address = request.remote_addr, hashed_ip  = hashed_ip)
 
 if __name__ == "__main__":
     app.run()
