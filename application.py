73ee83605288222e5edabcc767f970f56d831491
Peter Casinelli <pcas00@gmail.com>
2013-12-27 14:34:51 -0500

    Adding Angular JS and Bootstrap to application

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, make_response#render_template
 import hashlib
 
 application = Flask(__name__)
@@ -7,10 +7,14 @@ application.debug = True
 
 @application.route('/')
 def index():
+	return make_response(open('templates/index.html').read())
+
+"""@application.route('/')
+def index():
 	m = hashlib.md5()
 	m.update(request.remote_addr)
 	hashed_ip = m.hexdigest()
-	return render_template('index.html', ip_address = request.remote_addr, hashed_ip  = hashed_ip)
+	return render_template('index.html', ip_address = request.remote_addr, hashed_ip  = hashed_ip)"""
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
