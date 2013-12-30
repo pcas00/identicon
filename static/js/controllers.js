839eedaf4a84ff1b5f24ba71e1bab48d9a43c205
Peter Casinelli <pcas00@gmail.com>
2013-12-30 02:59:26 -0500

    Added ip and hash to views. Fixed bug so correct HTML5 identicon is shown

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -6,14 +6,12 @@ var identiconControllers = angular.module('identiconControllers', []);
 
 identiconControllers.controller('IdenticonCtrl', function ($scope, $http) {
 	$http.get('hash').success(function(data) {
-		$scope.hash = data;
+		$scope.hash = data.hash;
+		$scope.ipAddress = data.ipAddress;
 	});
 });
 
 identiconControllers.controller('GenerateIdenticonCtrl', function ($scope, $http) {
-	//$scope.ipAddress = '';
-	//$scope.hash = '';
-
 	$scope.generateIdenticon = function(ipAddress) {
 		$http.get('customhash?ipAddress='+ipAddress).success(function(data) {
 			$scope.hash = data.hash;
--- static/js/directives.js
+++ static/js/directives.js
@@ -3,10 +3,16 @@
 /* Directives */
 
 angular.module('identiconDirectives', []).
-  directive('identiconDisplay', function() {
+  directive('identiconDisplay', function($timeout) {
     return {
-    	link: function(scope, elm, attrs, ngModel) {
-    		angular.element(elm).identicon5({size:100});
+    	link: function(scope, elm, attrs) {
+    		/*I think I need to timeout in order for Angular to compile HTML.
+    		Without the timeout, the identicon5 jQuery will generate an identicon
+    		for a blank hash.
+    		*/
+    		$timeout(function() {
+    			angular.element(elm).identicon5({size:100});
+    		}, 82);
     	}
     };
   });
